453af51a101f96b3018e63b5ffdf81a7440e3f74
inflowencer <jason7bernhard@gmail.com>
2022-09-01 13:30:19 +0200

    Initiated setup files and github release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyaoa',
+    packages = ['pyaoa'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Python package to automatically analyze objects in external aerodynamic flow for a range of angle-of-attack',
+    author = 'inflowencer',
+    author_email = 'jason7bernhard@gmail.com',
+    url = 'https://github.com/inflowencer/pyaoa',
+    download_url = 'https://github.com/inflowencer/pyaoa/archive/refs/tags/v0.1.tar.gz',
+    keywords = ['cfd', 'pyaoa', 'external aerodynamics'],
+    classifiers = [],
+)
